4f7c31a90889e1dbb8141931876d37cd679bf8dd
PengMeng <383867421@qq.com>
2020-05-24 16:44:05 +0800

    deploy

---
+++ precache-manifest.61367561af0309e13def22d8e6c6d2be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e318f8646b00eb70dc83",
+    "url": "/morney-website/css/app.2394aef7.css"
+  },
+  {
+    "revision": "c73821482de4f173294f4ee824af1fed",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "e318f8646b00eb70dc83",
+    "url": "/morney-website/js/app.50fde5b3.js"
+  },
+  {
+    "revision": "aec2e51655686a98872f",
+    "url": "/morney-website/js/chunk-vendors.508d893e.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "035a1a16be03c15b9014dfcfcd20a86c",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
